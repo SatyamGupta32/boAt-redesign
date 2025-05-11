887c310d7fa432822b78009eed9fe5364be05191
Satyam Gupta <satyamg501@gmail.com>
2025-05-11 21:49:53 +0530

    first commit

--- src/components/Cart/Cart-tab/cartTab.jsx
+++ src/components/Cart/Cart-tab/cartTab.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import CartItem from '../Cart-item/cartItem';
 import { toggleStatusTab } from '../../../stores/cart';
-import './CartTab.css';
+import './cartTab.css';
 
 const CartTab = () => {
   const carts = useSelector(store => store.cart.items);
